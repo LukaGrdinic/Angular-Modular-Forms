99f800695301e6a7d60d7c6bd7d01b76e7d00a05
Luka Grdinic <luka@zlurad.me>
2021-09-03 10:04:25 +0200

    Make job-start date part of the job-details form

--- src/app/job-details/job-start-date/job-start-date.component.ts
+++ src/app/job-details/job-start-date/job-start-date.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, forwardRef, Input, OnChanges } from '@angular/core';
+import {
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+  NG_VALIDATORS,
+  FormGroup,
+  Validator,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+  FormBuilder
+} from '@angular/forms';
 
 @Component({
   selector: 'app-job-start-date',
   templateUrl: './job-start-date.component.html',
-  styleUrls: ['./job-start-date.component.scss']
+  styleUrls: ['./job-start-date.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => JobStartDateComponent),
+      multi: true
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => JobStartDateComponent),
+      multi: true
+    }
+  ]
 })
-export class JobStartDateComponent implements OnInit {
+export class JobStartDateComponent implements OnInit, ControlValueAccessor, Validator {
 
-  constructor() { }
+  get jobStartDate(): number {
+    return this.jobStartDateForm.get('date').value;
+  }
 
-  ngOnInit() {
+  public jobStartDateForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.jobStartDateForm = this.fb.group({
+      date: [ '', [ Validators.required ] ]
+    });
+  }
+
+  ngOnInit() {}
+
+  public onTouched: () => void = () => {};
+
+  writeValue(val: any): void {
+    // tslint:disable-next-line:no-unused-expression
+    val && this.jobStartDateForm.setValue(val, { emitEvent: false });
+  }
+  registerOnChange(fn: any): void {
+    console.log('on change');
+    this.jobStartDateForm.valueChanges.subscribe(fn);
+  }
+  registerOnTouched(fn: any): void {
+    console.log('on blur');
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    isDisabled ? this.jobStartDateForm.disable() : this.jobStartDateForm.enable();
+  }
+  validate(c: AbstractControl): ValidationErrors | null {
+    console.log('Job start date validation', c.errors);
+    return this.jobStartDateForm.valid ? null : { invalidForm: {valid: false, message: 'Job start date fields are invalid'}};
   }
 
 }
